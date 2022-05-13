cf2f014a5fc3fb92062222805cbc2d250d4b0af1
Sebastián García <garciaflorez.sebastian@gmail.com>
2022-05-12 21:42:55 -0500

    Creacion de hooks y helpers para una mejor organización del manejo de las peticiones, animaciones y css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,40 +1,24 @@
 import React, { useEffect, useState } from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem'
-
 
 export const GifGrid = ({ category }) => {
 
-  const [images, setImages] = useState([])
-
-  const getGifs = async () => {
-    const url = 'https://api.giphy.com/v1/gifs/search?limit=10&q="dragon+ball"&api_key=OAFoptQH39F7zVnnfQ9kQAHPY9QcmrjR'
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map(img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-      }
-    })
-    setImages(gifs)
-  }
-
-  useEffect(() => {
-    getGifs()
-  }, [])
-
+  const { data: images, loading } = useFetchGifs(category)
 
   return (
-    <div className='card-grid'>
+    <>
       <h5>{category}</h5>
-      {images.map(img => (
-        <GifGridItem
-          key={img.id}
-          img={img.url}
-          title={img.title}
-        />
-      ))}
-    </div>
+      {loading && <p className='card animate__animated animate__heartBeat'> Loading . . .</p>}
+      <div className='card-grid'>
+        {images.map(img => (
+          <GifGridItem
+            key={img.id}
+            img={img.url}
+            title={img.title}
+          />
+        ))}
+      </div>
+    </>
   )
 }
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ({ title, img }) => {
     console.log(title)
     return (
-        <div className='card'>
+        <div className='card animate__animated animate__heartBeat'>
             <img src={img} alt={title} />
             <p> {title} </p>
         </div>
